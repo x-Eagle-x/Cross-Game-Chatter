612895dc06e6e46f1be13fb5a39081cf0d6057b3
Adi Behluli <37238444+4D1G06@users.noreply.github.com>
2019-11-27 19:16:50 +0100

    Add files via upload

---
+++ Discord bot/bot.js
@@ -0,0 +1,32 @@
+var auth = require("./auth.json");
+const Discord = require("discord.js");
+const FileStream = require("fs");
+const Util = require("util");
+
+const bot = new Discord.Client({token: auth.token, autorun: true});
+bot.login(auth.token);
+
+const targetChannel = "YOUR CHANNEL ID";
+var lastMessage;
+
+FileStream.watch("discord_messages_o.txt", function (event, filename) {
+	if(filename && event == "change")
+		FileStream.readFile("discord_messages_o.txt", "utf8", function (err, data) {
+			if(err)
+				throw err;
+			
+			if(lastMessage != data)
+			{
+				bot.channels.get(targetChannel).send(data);
+				lastMessage = data;
+			}
+		});
+});
+
+bot.on("message", function sendMessages(msg) {
+	if(msg.author.bot)
+		return 0;
+	
+	FileStream.writeFile("discord_messages_i.txt", Util.format("[&x05%s&x01] &x04#%s&x01 %s&x01: %s", new Date().toLocaleTimeString(), msg.channel.name, (msg.author.bot ? "&x04BOT " : "&x05User ") + msg.author.username, msg), (err) => {
+    if (err) throw err;});
+})
