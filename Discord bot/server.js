9b1cca09624c84f2e3e3974d7ce8e387b5e89922
Adi Behluli <37238444+x-Eagle-x@users.noreply.github.com>
2020-05-20 15:55:46 +0200

    Update server.js

--- Discord bot/server.js
+++ Discord bot/server.js
@@ -1,10 +1,10 @@
 /*
-	Cross-Game Chatter v1.2
+	Cross-Game Chatter v1.3
 	By thEsp - https://github.com/4D1G06/Cross-Game-Chatter
 */
 
 const port = 1337;
-const targetChannel = "- ID HERE -";
+const targetChannel = "- ID GOES HERE-";
 
 const auth = require("./auth.json");
 const net = require("net");
@@ -24,7 +24,23 @@ function initializeServer()
 		socket.on("data", function(data)
 		{
 			message = data.toString("utf8");
-			bot.channels.get(targetChannel).send(message);
+
+			if (message.startsWith("Map"))
+			{
+    			bot.user.setStatus("online");
+    			bot.user.setPresence({
+        			game: {
+            			name: message,
+            			type: "Playing",
+            			afk: false,
+            			url: "https://github.com/x-Eagle-x/Cross-Game-Chatter/"
+        			}
+				});
+			}
+			else
+			{
+				bot.channels.get(targetChannel).send(message);			
+			}
 		});
 		
 		socket.on("close", function()
